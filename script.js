43b60c5b4d33da1205cacd9578ff3e20302217fb
arth <arthur.silvaaguiar8@gmail.com>
2026-05-28 20:49:32 -0300

    som nos cards e responsividade melhorada

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const cards = document.querySelectorAll('.card__inner');
+const cards = document.querySelectorAll('.card');
 
+cards.forEach(card => {
+  let audio = null;
+  let isPlaying = false;
 
-cards.forEach((card) =>{
-    card.addEventListener('click', function(){
-        card.classList.toggle('is-flipped');
-    });
+  // FLIP
+  card.addEventListener('click', () => {
+    card.classList.toggle('is-flipped');
+  });
+
+  const btn = card.querySelector('.btn');
+
+  btn.addEventListener('click', (event) => {
+    event.stopPropagation();
+
+    const audioSrc = card.dataset.audio;
+
+    if (!audio) {
+      audio = new Audio(audioSrc);
+    }
+
+    // se está tocando → pausa
+    if (isPlaying) {
+      audio.pause();
+      isPlaying = false;
+      return;
+    }
+
+    // se não está tocando → toca
+    audio.play();
+    isPlaying = true;
+
+    // quando acabar sozinho, reseta estado
+    audio.onended = () => {
+      isPlaying = false;
+    };
+  });
 });
